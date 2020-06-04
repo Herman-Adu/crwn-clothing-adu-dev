a78a8ba11b9c16244b901d4ac9e80f6c19c83b3a
Herman Adu <herman.adu@hotmail.com>
2020-06-04 10:20:49 +0100

    updated the homepage component test

--- client/src/pages/homepage/homepage.test.js
+++ client/src/pages/homepage/homepage.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Homepage from './homepage.component';
+import HomePage from './homepage.component';
 
 it('should render Homepage component', () => {
-    expect(shallow(<Homepage />)).toMatchSnapshot();
+    expect(shallow(<HomePage />)).toMatchSnapshot();
 });
