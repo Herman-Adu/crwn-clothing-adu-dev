4a5af1f885bff3c8088a2f51aaec9ee4c3525827
Herman Adu <herman.adu@hotmail.com>
2020-06-06 13:37:05 +0100

    added unit tests to check that the ShopPage component renders correctly

--- client/src/pages/shop/shop.test.js
+++ client/src/pages/shop/shop.test.js
@@ -11,58 +11,59 @@ export const createMockStore = ({ state, reducers }) => {
     return {
         ...store,
         persistor: {
-        persist: () => null
+            persist: () => null
         }
     };
-    };
+};
 
-    describe('ShopPage', () => {
-        let wrapper;
-        let mockFetchCollectionsStart;
-        let store;
+describe('ShopPage', () => {
+    let wrapper;
+    let mockFetchCollectionsStart;
+    let store;
 
-        beforeEach(() => {
-            const mockReducer = (
-                state = {
-                    isFetching: true
-            },
-            action) => state;
+    beforeEach(() => {
+        const mockReducer = (
+            state = {
+                isFetching: true
+        },
+        action) => state;
 
-            const mockState = {
-                shop: {
-                    isFetching: true
-                }
-            };
+        const mockState = {
+            shop: {
+                isFetching: true
+            }
+        };
 
-            mockFetchCollectionsStart = jest.fn();
+        mockFetchCollectionsStart = jest.fn();
 
-            store = createMockStore({
-                state: mockState,
-                reducers: { shop: mockReducer }
-            });
-
-            const mockMatch = {
-                path: ''
-            };
-
-            const mockProps = {
-                match: mockMatch,
-                fetchCollectionsStart: mockFetchCollectionsStart
-            };
-
-            wrapper = mount(
-                <BrowserRouter>
-                    <Provider store={store}>
-                        <ShopPage {...mockProps} />
-                    </Provider>
-                </BrowserRouter>
-            );
+        store = createMockStore({
+            state: mockState,
+            reducers: { shop: mockReducer }
         });
 
-        it('should render ShopPage component', () => {
-            expect(wrapper).toMatchSnapshot();
-        });
+        const mockMatch = {
+            path: ''
+        };
 
+        const mockProps = {
+            match: mockMatch,
+            fetchCollectionsStart: mockFetchCollectionsStart
+        };
 
+        wrapper = mount(
+            <BrowserRouter>
+                <Provider store={store}>
+                    <ShopPage {...mockProps} />
+                </Provider>
+            </BrowserRouter>
+        );
+    });
 
+    it('should render ShopPage component', () => {
+        expect(wrapper).toMatchSnapshot();
+    });    
+
+    it('should render ShopPage component', () => {
+        expect(mockFetchCollectionsStart).toHaveBeenCalled();
+    });
 });
