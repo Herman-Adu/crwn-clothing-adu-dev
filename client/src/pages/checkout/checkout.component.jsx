f292dcb6ad21b120d3ed9d4241397778efb55669
Herman Adu <herman.adu@hotmail.com>
2020-06-18 12:01:34 +0100

    updating the credit card information

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -41,7 +41,7 @@ export const CheckoutPage = ({ cartItems, total }) => (
             <WarningContainer>
                 *Please use the following test credit card for payments*
                 <br />
-                4242 4242 4242 4242 - Exp: 05/20 - CVV: 123
+                4242 4242 4242 4242 - Exp: 12/20 - CVV: 123
             </WarningContainer>
         <StripeCheckoutButton price={total} />
     </CheckoutPageContainer>
